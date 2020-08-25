8860fd4f918e8b7b0e65ed4f2c6416f00a3e0954
Tessho Nakamura <tesstesstex@gmail.com>
2020-08-25 21:43:07 +0900

    chapter 1.6.4 std::string

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <iostream>
 
 void show_value(float f)
@@ -7,12 +8,9 @@ void show_value(float f)
 
 int main()
 {
-  char hello[] = "Hello"; // 文字列リテラルで初期化
-
-  char array[6] = {'a', 'r', 'r'}; // 文字リテラルの配列として初期化
-  array[3] = 'a';
-  array[4] = 'y';
-  array[5] = '\0'; // ヌル文字
-
-  std::cout << hello << ", " << array << std::endl;
+  // std::string という型を使うと配列でなくても文字列を使える
+  std::string hello = "Hello";
+  std::cout << hello;
+  hello = ", string"; // より長い文字を代入できる
+  std::cout << hello << std::endl;
 }
