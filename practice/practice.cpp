555197f4d5ccf06d631b69759f1f29a8d0c6c22d
Tessho Nakamura <tesstesstex@gmail.com>
2020-08-25 21:54:51 +0900

    Added practice.cpp

---
+++ practice/practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  int array[] = {4, 2, 1, 9, 5};
+
+  std::cout << /* hogehoge */ << std::endl;
+  std::cout << /* hogehoge */ << std::endl;
+  std::cout << /* hogehoge */ << std::endl;
+  std::cout << /* hogehoge */ << std::endl;
+  std::cout << /* hogehoge */ << std::endl;
+}
