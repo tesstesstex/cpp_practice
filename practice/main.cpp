4e85783b8d9057700abea8d34ab9317c0c03f358
Tessho Nakamura <tesstesstex@gmail.com>
2020-08-31 11:05:02 +0900

    chapter 2.1.2

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 
-// 2.1.1 構造体 struct
-struct product
+struct S
 {
-  int id;
-  int price;
-  int stock;
+  int a;
+  int b;
+  int c;
 };
 
-void show_product(product product)
+union U
 {
-  std::cout << "商品ID: " << product.id << std::endl;
-  std::cout << "単価: " << product.price << std::endl;
-  std::cout << "在庫数: " << product.stock << std::endl;
-}
+  int d;
+  int e;
+  int f;
+};
 
 int main()
 {
-  product pen =
-  {
-    0,
-    100,
-    200,
-  };
+  S s;
 
-  show_product(pen);
+  std::cout << "Struct---" << std::endl;
+  std::cout << "the address of s.a is " << &s.a << std::endl;
+  std::cout << "the address of s.b is " << &s.b << std::endl;
+  std::cout << "the address of s.c is " << &s.c << std::endl;
+
+  U u = { 42 }; // 初期化は先頭メンバーのみ
+
+  std::cout << "Union---" << std::endl;
+  std::cout << "the address of u.d is " << &u.d << std::endl;
+  std::cout << "the address of u.e is " << &u.e << std::endl;
+  std::cout << "the address of u.f is " << &u.f << std::endl;
+
+  std::cout << "the value of u.d is " << u.d << std::endl;
+  std::cout << "the value of u.e is " << u.e << std::endl;
+  std::cout << "the value of u.f is " << u.f << std::endl;
+
+  u.f = 1024;
+
+  std::cout << "the value of u.d is " << u.d << std::endl;
+  std::cout << "the value of u.e is " << u.e << std::endl;
+  std::cout << "the value of u.f is " << u.f << std::endl;
 }
