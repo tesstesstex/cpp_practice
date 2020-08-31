46fd9a7561aa80301be4a84173dd5fe2d820c3b4
Tessho Nakamura <tesstesstex@gmail.com>
2020-08-31 11:19:00 +0900

    chapter 2.1.3 C lang 形式では enum enum-name {   enumeration1,   enumeration2 = value, };

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,42 +1,19 @@
 #include <iostream>
 
-struct S
+// 列挙体
+enum class Category
 {
-  int a;
-  int b;
-  int c;
-};
-
-union U
-{
-  int d;
-  int e;
-  int f;
+  Value1, // 0
+  Value2, // 1
+  Value3 = 100,
+  Value4, // 101
 };
 
 int main()
 {
-  S s;
+  // 列挙体の変数を宣言してValue3で初期化
+  // 列挙体名と列挙値をスコープ解決演算子::でつなげるとその値を使うことができる
+  Category cat = Category::Value3;
 
-  std::cout << "Struct---" << std::endl;
-  std::cout << "the address of s.a is " << &s.a << std::endl;
-  std::cout << "the address of s.b is " << &s.b << std::endl;
-  std::cout << "the address of s.c is " << &s.c << std::endl;
-
-  U u = { 42 }; // 初期化は先頭メンバーのみ
-
-  std::cout << "Union---" << std::endl;
-  std::cout << "the address of u.d is " << &u.d << std::endl;
-  std::cout << "the address of u.e is " << &u.e << std::endl;
-  std::cout << "the address of u.f is " << &u.f << std::endl;
-
-  std::cout << "the value of u.d is " << u.d << std::endl;
-  std::cout << "the value of u.e is " << u.e << std::endl;
-  std::cout << "the value of u.f is " << u.f << std::endl;
-
-  u.f = 1024;
-
-  std::cout << "the value of u.d is " << u.d << std::endl;
-  std::cout << "the value of u.e is " << u.e << std::endl;
-  std::cout << "the value of u.f is " << u.f << std::endl;
+  std::cout << static_cast<int>(cat) << std::endl;
 }
