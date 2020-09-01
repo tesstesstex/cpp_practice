97a5c48bf6f1c10bf34a21632ebc7070e968f2f6
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-01 23:34:03 +0900

    chapter 2.3.2 参照は変更できない。 変更しようとするともとの参照先の値が変わってしまう。

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,19 +1,27 @@
 // 2.3 参照
-// 2.3.1 変数の別名
+// 2.3.2 参照とポインターの違い
 
 #include <iostream>
 
 int main()
 {
   int value = 42;
+  int other = 0;
+  int* pointer = &value; // value へのポインター
+  int& reference = value; // value への参照
 
-  std::cout << "valuable value's address is " << &value << " and its value is " << value << "." << std::endl;
+  std::cout << "value's address is " << &value << " and its value is " << value << "." << std::endl;
+  std::cout << "other's address is " << &other << " and its value is " << other << "." << std::endl;
+  std::cout << "pointer's address is " << pointer << " and its value is " << *pointer << "." << std::endl;
+  std::cout << "reference's address is " << &reference << " and its value is " << reference << "." << std::endl;
 
-  int& reference = value;
+  pointer = &other; // ポインターが持つアドレスをother のアドレスへ変更
+  reference = other; // 参照先をvalue からother へ変えたつもり
 
-  std::cout << "valuable reference's address is " << &reference << " and its value is " << reference << "." << std::endl;
+  std::cout << std::endl; // 空行
+  std::cout << "value's address is " << &value << " and its value is " << value << "." << std::endl;
+  std::cout << "other's address is " << &other << " and its value is " << other << "." << std::endl;
+  std::cout << "pointer's address is " << pointer << " and its value is " << *pointer << "." << std::endl;
+  std::cout << "reference's address is " << &reference << " and its value is " << reference << "." << std::endl;
 
-  reference = 0;
-
-  std::cout << "valuable value's address is " << &value << " and its value is " << value << "." << std::endl;
 }
