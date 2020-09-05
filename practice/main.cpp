ac69e64c6f7d9b2a1107d6eb923fd24f78ed0d42
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-05 12:56:13 +0900

    chapter 2.5.2

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,13 +1,40 @@
 // 2.5 型の別名定義
-// 2.5.1 型に別の名前を与える
+// 2.5.2 ネストした型名
 
 #include <iostream>
 
-using integer = int;
-integer main()
+class data
 {
-  int value = 42;
-  using number = integer;
-  number num = 0;
-  value = num;
+  public:
+      using integer = int;
+
+      integer get_value();
+      void set_value(integer new_value);
+  private:
+      integer value;
+};
+
+// クラスの外なのでスコープ解決演算子を使用
+data::integer data::get_value()
+{
+  return value;
+}
+
+// メンバー関数の中はクラスの内側なので、スコープ解決演算子を使う必要はない
+void data::set_value(integer new_value)
+{
+  integer tmp = new_value;
+  value = tmp;
+}
+
+int main()
+{
+  data d;
+
+  using integer = data::integer;
+
+  integer value = 42;
+  d.set_value(value);
+
+  std::cout << d.get_value() << std::endl;
 }
