b3f061d60c13166462dd4c2ee86fd361bca6aef3
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-05 12:30:50 +0900

    chapter 2.4.3

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,13 +1,19 @@
 // 2.4 型推論
-// 2.4.2 範囲for 文の型推論
+// 2.4.3 式の型を推論する
 
 #include <iostream>
 
+// 値1 の型を推論し、その方を戻り値の型として使う
+decltype(1) one()
+{
+  return 1;
+}
+
 int main()
 {
-  int array[] = {0, 1, 2, 3, 4};
-  for (auto e : array)
-  {
-    std::cout << e << std::endl;
-  }
+  auto i = one();
+  decltype(i) j;
+
+  j = 42.195;
+  std::cout << j << std::endl;
 }
