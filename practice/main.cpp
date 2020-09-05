07a16400af60dc7bee9416faf34c65c447567d8b
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-05 12:11:51 +0900

    chapter 2.4.1 型推論ってほぼ使わなさそう。

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,27 +1,23 @@
-// 2.3 参照
-// 2.3.2 参照とポインターの違い
+// 2.4 型推論
+// 2.4.1 変数の方と初期値の型
 
 #include <iostream>
+#include <typeinfo>
+
+int one()
+{
+  return 1;
+}
 
 int main()
 {
-  int value = 42;
-  int other = 0;
-  int* pointer = &value; // value へのポインター
-  int& reference = value; // value への参照
+  auto d = 3.14;
+  d = 2.71f;
+  std::cout << typeid(d).name() << std::endl;
+  std::cout << d << std::endl;
 
-  std::cout << "value's address is " << &value << " and its value is " << value << "." << std::endl;
-  std::cout << "other's address is " << &other << " and its value is " << other << "." << std::endl;
-  std::cout << "pointer's address is " << pointer << " and its value is " << *pointer << "." << std::endl;
-  std::cout << "reference's address is " << &reference << " and its value is " << reference << "." << std::endl;
-
-  pointer = &other; // ポインターが持つアドレスをother のアドレスへ変更
-  reference = other; // 参照先をvalue からother へ変えたつもり
-
-  std::cout << std::endl; // 空行
-  std::cout << "value's address is " << &value << " and its value is " << value << "." << std::endl;
-  std::cout << "other's address is " << &other << " and its value is " << other << "." << std::endl;
-  std::cout << "pointer's address is " << pointer << " and its value is " << *pointer << "." << std::endl;
-  std::cout << "reference's address is " << &reference << " and its value is " << reference << "." << std::endl;
-
+  auto i = one();
+  i = 42.195;
+  std::cout << typeid(i).name() << std::endl;
+  std::cout << i << std::endl;
 }
