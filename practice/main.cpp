29cad5699db8de84a915ca223d0ad70173b72808
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-05 12:19:25 +0900

    chapter 2.4.2

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,23 +1,13 @@
 // 2.4 型推論
-// 2.4.1 変数の方と初期値の型
+// 2.4.2 範囲for 文の型推論
 
 #include <iostream>
-#include <typeinfo>
-
-int one()
-{
-  return 1;
-}
 
 int main()
 {
-  auto d = 3.14;
-  d = 2.71f;
-  std::cout << typeid(d).name() << std::endl;
-  std::cout << d << std::endl;
-
-  auto i = one();
-  i = 42.195;
-  std::cout << typeid(i).name() << std::endl;
-  std::cout << i << std::endl;
+  int array[] = {0, 1, 2, 3, 4};
+  for (auto e : array)
+  {
+    std::cout << e << std::endl;
+  }
 }
