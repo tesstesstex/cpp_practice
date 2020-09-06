64c54fd0a55ad697c352ed14b4f51a44b37cc312
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-07 08:57:16 +0900

    chapter 2.8.4

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,17 +1,18 @@
 // 2.8 ラムダ式
-// 2.8.3 変数のコピーキャプチャ
+// 2.8.4 変更可能なコピーキャプチャ
 
 #include <iostream>
 
 int main()
 {
   int a = 0;
-  auto lambda = [a]()
+
+  auto lambda = [a]() mutable
   {
+    a = 42;
     std::cout << a << std::endl;
   };
-  lambda();
 
-  a = 42;
   lambda();
+  std::cout << a << std::endl;
 }
