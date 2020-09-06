a416bce1423bfa12d9b5144d7dc1ce0fb518629a
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-07 08:54:24 +0900

    chapter 2.8.3 lambda 式はクロージャとも言う。

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,46 +1,17 @@
-// 2.7 関数オーバーロード
-// 2.7.2 型が違うオーバーロード
+// 2.8 ラムダ式
+// 2.8.3 変数のコピーキャプチャ
 
 #include <iostream>
 
-// 2 次元ベクトルを表す構造体
-struct vector2d
-{
-  int x;
-  int y;
-};
-
-// 整数用のオーバーロード
-int add(int left, int right)
-{
-  return left + right;
-}
-
-// 浮動小数点用のオーバーロード
-double add(double left, double right)
-{
-  return left + right;
-}
-
-// 2 次元ベクトル用のオーバーロード
-vector2d add(vector2d left, vector2d right)
-{
-  vector2d v;
-  v.x = left.x + right.x;
-  v.y = left.y + right.y;
-  return v;
-}
-
 int main()
 {
-  int integer = add(1, 2);
-  std::cout << integer << std::endl;
+  int a = 0;
+  auto lambda = [a]()
+  {
+    std::cout << a << std::endl;
+  };
+  lambda();
 
-  double floating = add(3.14, 42.195);
-  std::cout << floating << std::endl;
-
-  vector2d v = {1, 2};
-  vector2d u = {-3, 4};
-  vector2d w = add(v, u);
-  std::cout << w.x << ", " << w.y << std::endl;
+  a = 42;
+  lambda();
 }
