c6d16f45cf2ee9fbf77210f0b6a0beb31ff7871c
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-06 18:17:18 +0900

    chapter 2.6.2 getline を使うと行全体の入力を受け取れる。

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,11 +1,17 @@
 // 2.6 コンソールからの入力
-// 2.6.1 std::cin
+// 2.6.2 文字列の入力
 #include <iostream>
+#include <string>
 
 int main()
 {
-  int i;
-  std::cout << "Please input number.> ";
-  std::cin >> i;
-  std::cout << "You inputed \"" << i << "\"." << std::endl;
+  /* std::string s; */
+  /* std::cout << "文字列を入力しなさい！> "; */
+  /* std::cin >> s; */
+  /* std::cout << "入力された文字列は\"" << s << "\"です" << std::endl; */
+
+  std::string sl;
+  std::cout << "文字列を入力しなさい！> ";
+  std::getline(std::cin, sl); // 1行全体の文字列入力を受け取る
+  std::cout << "入力された文字列は\"" << sl << "\"です" << std::endl;
 }
