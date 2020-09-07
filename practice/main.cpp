468b94e34b3bc686315f58714774fba1bf55448b
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-08 05:46:26 +0900

    chapter 3.1.3

--- practice/main.cpp
+++ practice/main.cpp
@@ -4,30 +4,56 @@
 
 #include <iostream>
 
-class product
+class heavy_class
 {
-  int id;
+  int m_value;
+
+  mutable int m_cache;
+  mutable bool m_cache_valid;
 
   public:
-    int get_id();
-    int get_id() const;
+    int generate() const;
+    void set(int value);
+    int get() const;
 };
 
-int product::get_id()
+int heavy_class::generate() const
 {
-  std::cout << "not const" << std::endl;
+  std::cout << "function to create very heavy data" << std::endl;
+  return m_value;
 }
 
-int product::get_id() const
+void heavy_class::set(int value)
 {
-  std::cout << "const" << std::endl;
+  // 本来はセットされたタイミングで最終的に使うデータを生成できるとよいが、
+  // 処理が重い場合には必要になるまで生成しないこともある
+  m_cache_valid = false; // キャッシュを無効化
+  m_value = value;
+}
+
+int heavy_class::get() const
+{
+  // キャッシュが有効ならそれを返す
+  if(m_cache_valid) { return m_cache; }
+
+  // 本来はconst メンバー関数はメンバー変数を変更できないが
+  // mutable なメンバー変数は変更できる
+  m_cache = generate(); // データを生成してキャッシュに保存
+  m_cache_valid = true; // キャッシュを有効化
+
+  return m_cache;
 }
 
 int main()
 {
-  product p;
-  p.get_id();
+  heavy_class heavy_object;
+  heavy_object.set(100);
+  std::cout << heavy_object.get() << std::endl;
+  // データ生成関数は呼ばれずにキャッシュが返される
+  std::cout << heavy_object.get() << std::endl;
 
-  const product cp{};
-  cp.get_id();
+  heavy_object.set(200);
+  std::cout << heavy_object.get() << std::endl;
+  // データ生成関数は呼ばれずにキャッシュが返される
+  std::cout << heavy_object.get() << std::endl;
 }
