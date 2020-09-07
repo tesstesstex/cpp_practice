1afe0862b3e004aed205df12713226c571a1c195
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-08 05:14:03 +0900

    chapter 3.1.1

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,21 +1,29 @@
-// 2.8 ラムダ式
-// 2.8.5 参照を取得するキャプチャ
+// 3 クラス
+// 3.1 const メンバー関数とmutable
+// 3.1.1 メンバー変数を変更する/しない関数
 
 #include <iostream>
 
+class product
+{
+  int id;
+  public:
+    int get_id() const;
+    void set_id(int new_id);
+};
+
+int product::get_id() const
+{
+  return id;
+}
+
+void product::set_id(int new_id)
+{
+  id = new_id;
+}
+
 int main()
 {
-  int a = 0;
-
-  auto lambda = [&a]()
-  {
-    std::cout << a << std::endl;
-    ++a;
-  };
-
-  lambda();
-  // original
-  std::cout << a << std::endl;
-  a = 42;
-  lambda();
+  const product cp{};
+  std::cout << cp.get_id() << std::endl;
 }
