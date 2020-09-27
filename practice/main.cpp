ea17550ac44c13a01655d98c5fed88e9e9b1cb1d
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-27 23:53:36 +0900

    chapter 3.3.2

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,6 +1,6 @@
 // 3 クラス
-// 3.2 コンストラクターとデストラクタ〜
-// 3.2.4 デストラクター
+// 3.3 初期値を受け取るコンストラクター
+// 3.3.2 コンストラクターの委譲
 
 #include <iostream>
 #include <string>
@@ -8,11 +8,13 @@
 class person
 {
   std::string m_name;
-  int          m_age;
+  int         m_age;
+
+  person(int age);
 
   public:
     person();
-    ~person();
+    person(std::string name, int age);
 
     void set_name(std::string name);
     void set_age(int age);
@@ -21,14 +23,29 @@ class person
     int         age() const;
 };
 
-person::person() : m_age(-1)
+// 共通な初期化処理が書かれたコンストラクター
+person::person(int age) : m_age(age)
 {
-  std::cout << "コンストラクター呼び出し" << std::endl;
+  // 複数のコンストラクターで共通な大本の処理をprivateなコンストラクターに用意し、
+  // 委譲コンストラクターを使うことで、一貫した漏れのない初期化手順を提供できる
+  std::cout << "共通コンストラクター呼び出し" << std::endl;
 }
 
-person::~person()
+// 委譲元コンストラクター（引数なし）
+person::person()
+  : person(-1) // 委譲先コンストラクター
 {
-  std::cout << "デストラクター呼び出し" << std::endl;
+  std::cout << "引数なしコンストラクター呼び出し" << std::endl;
+}
+
+// 委譲元コンストラクター（名前と年齢を与えて初期化する）
+person::person(std::string name, int age)
+  : person(age) // 委譲先コンストラクター
+{
+  std::cout << "引数付きコンストラクター呼び出し" << std::endl;
+
+  // 初期化中にメンバー関数を呼び出すこともできる
+  set_name(name);
 }
 
 void person::set_name(std::string name)
@@ -53,10 +70,9 @@ int person::age() const
 
 int main()
 {
-  person bob;
-  bob.set_name("bob");
-  bob.set_age(20);
-  std::cout << "main() 関数の最後" << std::endl;
+  // 引数を渡して初期化
+  person alice("alice", 15);
 
-  // bob はもう使われないのでデストラクターが呼ばれる
+  std::cout << alice.name() << std::endl;
+  // alice と表示
 }
