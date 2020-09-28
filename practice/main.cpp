20a76fbda0e2f40a77a4e2ea067c09f6bbbf654d
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-28 22:35:55 +0900

    chapter 3.4.2

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,19 +1,45 @@
 // 3 クラス
 // 3.4 デフォルトの初期値
-// 3.4.1 メンバー変数の初期値
+// 3.4.2 メンバー初期化リストと初期値
 
 #include <iostream>
+#include <string>
 
-class S
+class Book
 {
+  std::string title = "";
+  int         page = 0;
+
   public:
-    int answer = 42; // NSDMI
-    float pi{ 3.14159265f }; // NSDMI
+    Book();
+    explicit Book(std::string title, int page);
+
+    void show() const;
 };
+
+// page はメンバー初期化リストで省略したのでNSDMI のデフォルト値を使って初期化される
+Book::Book() : title("untitled")
+{
+
+}
+
+// メンバー初期化リストで与えた初期値が使われる
+Book::Book(std::string title, int page) : title(title), page(page)
+{
+
+}
+
+void Book::show() const
+{
+  std::cout << title << " " << page << "ページ" << std::endl;
+}
 
 int main()
 {
-  S s; // NSDMI によってデフォルト値で初期化される
-  std::cout << "s.answer: " << s.answer << std::endl;
-  std::cout << "s.pi: " << s.pi << std::endl;
+  Book nsdmi;
+
+  nsdmi.show();
+  Book old_edition{"独習C++ 第4版", 568};
+
+  old_edition.show();
 }
