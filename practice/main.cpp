3d67be9eaee6a16f7eb00e86298a903c64543c8c
Tessho Nakamura <tesstesstex@gmail.com>
2020-09-28 22:24:17 +0900

    chapter 3.4.1

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,40 +1,19 @@
 // 3 クラス
-// 3.3 初期値を受け取るコンストラクター
-// 3.3.5 explicit 指定子
+// 3.4 デフォルトの初期値
+// 3.4.1 メンバー変数の初期値
 
 #include <iostream>
-#include <string>
 
-class A
+class S
 {
-  int m_v;
-
   public:
-    explicit A(int); // explicit キーワードを追加することで
-                     // 暗黙のコンストラクター呼び出しを禁止できる
-    int v() const;
+    int answer = 42; // NSDMI
+    float pi{ 3.14159265f }; // NSDMI
 };
-
-A::A(int v) : m_v(v) // 定義にはexplicit を書かない
-{
-
-}
-
-int A::v() const
-{
-  return m_v;
-}
 
 int main()
 {
-  /* A x = 42; // エラー。暗黙のコンストラクター呼び出しは禁止されている */
-  A y(42); // OK. 明示的なコンストラクター呼び出し
-  if (y.v() == 42)
-  {
-    std::cout << "A.v() は42 です" << std::endl;
-  }
-  else
-  {
-    std::cout << "A.v() は42 ではない" << std::endl;
-  }
+  S s; // NSDMI によってデフォルト値で初期化される
+  std::cout << "s.answer: " << s.answer << std::endl;
+  std::cout << "s.pi: " << s.pi << std::endl;
 }
